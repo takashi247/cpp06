5e4cd082d1b55fe00fc5e2d8656b031eda36c565
takashi247 <takashi247@gmail.com>
2022-02-03 11:59:55 +0900

    update main based on StringConverter class

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -6,10 +6,11 @@ int main(int ac, char **av) {
     std::cerr << "Error: Please provide one argument" << std::endl;
     exit(EXIT_FAILURE);
   }
-
-  std::string str = av[1];
+  const std::string str = av[1];
   StringConverter s_converter(str);
-
+  s_converter.setValueType();
+  s_converter.createBaseValue();
+  s_converter.convertToOtherTypes();
   s_converter.printValues();
 }
 
