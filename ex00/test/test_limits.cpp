6a1a62dc5916252f6d52064cf627e06f29a32c95
takashi247 <takashi247@gmail.com>
2022-02-04 17:26:59 +0900

    modify test_limits

--- ex00/test/test_limits.cpp
+++ ex00/test/test_limits.cpp
@@ -8,11 +8,11 @@ void showMinMax() {
     std::cout << "min: " << static_cast<int>(std::numeric_limits<T>::min()) << std::endl;
     std::cout << "max: " << static_cast<int>(std::numeric_limits<T>::max()) << std::endl;
   } else if (typeid(T) == typeid(float)) {
-    std::cout << "min: " << std::fixed << std::setprecision(1) << static_cast<int>(std::numeric_limits<T>::min()) << std::endl;
-    std::cout << "max: " << std::fixed << std::setprecision(150) << static_cast<int>(std::numeric_limits<T>::max()) << std::endl;
+    std::cout << "min: " << std::fixed << std::setprecision(150) << std::numeric_limits<T>::min() << std::endl;
+    std::cout << "max: " << std::fixed << std::setprecision(1) << std::numeric_limits<T>::max() << std::endl;
   } else if (typeid(T) == typeid(double)) {
-    std::cout << "min: " << std::fixed << std::setprecision(1) << static_cast<int>(std::numeric_limits<T>::min()) << std::endl;
-    std::cout << "max: " << std::fixed << std::setprecision(1050) << static_cast<int>(std::numeric_limits<T>::max()) << std::endl;
+    std::cout << "min: " << std::fixed << std::setprecision(1050) << std::numeric_limits<T>::min() << std::endl;
+    std::cout << "max: " << std::fixed << std::setprecision(1) << std::numeric_limits<T>::max() << std::endl;
   } else {
     std::cout << "min: " << std::numeric_limits<T>::min() << std::endl;
     std::cout << "max: " << std::numeric_limits<T>::max() << std::endl;
@@ -24,8 +24,6 @@ int main() {
 
   std::cout << "char:" << std::endl;
   showMinMax<char>();
-  std::cout << "short:" << std::endl;
-  showMinMax<short>();
   std::cout << "int:" << std::endl;
   showMinMax<int>();
   std::cout << "long:" << std::endl;
@@ -34,12 +32,4 @@ int main() {
   showMinMax<float>();
   std::cout << "double:" << std::endl;
   showMinMax<double>();
-  std::cout << "long double:" << std::endl;
-  showMinMax<long double>();
-  std::cout << "unsigned short:" << std::endl;
-  showMinMax<unsigned short>();
-  std::cout << "unsigned int:" << std::endl;
-  showMinMax<unsigned int>();
-  std::cout << "unsigned long:" << std::endl;
-  showMinMax<unsigned long>();
 }
