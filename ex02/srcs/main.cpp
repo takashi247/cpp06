6d0e97defacaf04bce3bb04e0f4be3e6c5454ec4
takashi247 <takashi247@gmail.com>
2022-02-06 09:10:38 +0900

    update main function

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -1,8 +1,12 @@
 #include "Identifier.hpp"
 
+#include <iostream>
+
 int main() {
   Base* random_instance = generate();
+  std::cout << "Check 'void identify(Base* p)': ";
   identify(random_instance);
+  std::cout << "Check 'void identify(Base& p)': ";
   identify(*random_instance);
   delete random_instance;
 }
